2841a9ada98cfd8f7d5380101442fcc87ad78441
James <jamestariga@live.com>
2022-01-22 18:28:17 -0800

    Added icons

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
-const API = YOU_API_KEY
+import { API } from './api.js'
+
+const API_KEY = API.key
 
 window.addEventListener('load', async () => {
     if (navigator.geolocation) {
@@ -6,8 +8,11 @@ window.addEventListener('load', async () => {
             let { latitude, longitude } = position.coords
 
             try {
-                const res = await axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=metric&appid=${API}`)
-                getData(res)
+                const resCurrent = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${API_KEY}`)
+                getCurrent(resCurrent.data)
+                getImage(resCurrent.data.weather[0].icon)
+                const resFuture = await axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=metric&appid=${API_KEY}`)
+                getFuture(resFuture.data)
             } catch (err) {
                 console.error(err)
             }
@@ -15,23 +20,75 @@ window.addEventListener('load', async () => {
     }
 })
 
-const getData = (response) => {
-    console.log(response)
+const getImage = (data) => {
+    console.log(data)
+    const weatherIcon = document.querySelector('.weather-icon')
+    const img = document.createElement('img')
+
+    if(data == '01d') {
+        img.src = './img/sunny.png'
+        weatherIcon.appendChild(img)
+    } else if(data == '01n') {
+        img.src = './img/full-moon.png'
+        weatherIcon.appendChild(img)
+    } else if(data == '02d') {
+        img.src = './img/partly-cloudy.png'
+        weatherIcon.appendChild(img)
+    } else if(data == '02n') {
+        img.src = './img/night.png'
+        weatherIcon.appendChild(img)
+    } else if(data == '03d' || data == '03n') {
+        img.src = './img/scattered-cloud.png'
+        weatherIcon.appendChild(img)
+    } else if(data == '04d' || data == '04n') {
+        img.src = './img/heavy-clouds.png'
+        weatherIcon.appendChild(img)
+    } else if(data == '09d') {
+        img.src = './img/rainfall.png'
+        weatherIcon.appendChild(img)
+    } else if(data == '09n' || data == '10n') {
+        img.src = './img/rainy-night.png'
+        weatherIcon.appendChild(img)
+    } else if(data == '10d') {
+        img.src = './img/moderate-rain.png'
+        weatherIcon.appendChild(img)
+    } else if(data == '11d') {
+        img.src = './img/storm-heavy-rain.png'
+        weatherIcon.appendChild(img)
+    } else if(data == '11n') {
+        img.src = './img/stormy-night.png'
+        weatherIcon.appendChild(img)
+    } else if(data == '13d' || data == '13n') {
+        img.src = './img/snow.png'
+        weatherIcon.appendChild(img)
+    } else if(data == '50d' || data == '50n') {
+        img.src = './img/fog.png'
+        weatherIcon.appendChild(img)
+    }
+}
+
+const getCurrent = (data) => {
+    console.log(data)
     const displayCity = document.querySelector('.city')
     const displayTemperature = document.querySelector('.temperature')
     const displayWeather = document.querySelector('.weather')
     const displayHumidity = document.querySelector('.humidity')
 
-    const { temp, humidity } = response.data.current
+    const location = data.name
+    const country = data.sys.country
 
-    const { timezone } = response.data
+    const { temp, humidity } = data.main
 
-    const weather = response.data.current.weather[0].main
+    const weather = data.weather[0].description
 
     const tempCel = Math.round((temp))
 
-    displayCity.textContent = timezone
+    displayCity.textContent = `${location}, ${country}`
     displayTemperature.textContent = tempCel + '°'
     displayWeather.textContent = 'Current: ' + weather
     displayHumidity.textContent = 'Humidity: ' + humidity
+}
+
+const getFuture = (response) => {
+    console.log(response)
 }
